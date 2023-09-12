ff4f0c456176e83b9a1ea3a90c0dd203681d4fe3
Jakaria Istauk <jakariamd35@gmail.com>
2023-09-12 09:34:06 +0600

    init the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/common/Header'
+import Products from './components/layouts/product/Products'
+import Footer from './components/common/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Products/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
